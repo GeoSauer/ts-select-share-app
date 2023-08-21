b18fd895b8baba057b886fa97b6ec0b755d70320
GeoSauer <geoffrey.sauer89@gmail.com>
2023-08-21 17:30:19 -0600

    adds map rendering with marker on chosen location

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+const API_KEY = process.env.GOOGLE_API_KEY;
 
 //* this isn't every value in the response, just the data that we need for the app
 type GoogleGeocodingResponse = {
@@ -19,14 +19,18 @@ function searchAddressHandler(event: Event) {
     .get<GoogleGeocodingResponse>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
-      )}&key=${GOOGLE_API_KEY}`
+      )}&key=${API_KEY}`
     )
     .then((response) => {
       if (response.data.status !== "OK") {
         throw new Error("Could not fetch location!");
       }
       const coordinates = response.data.results[0].geometry.location;
-      console.log(coordinates);
+      const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: coordinates,
+        zoom: 16,
+      });
+      new google.maps.Marker({ position: coordinates, map: map });
     })
     .catch((error) => {
       alert(error.message);
